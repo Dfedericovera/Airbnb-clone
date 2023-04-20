8fbc573777e71399023016bb66e94e70593defd5
Federico Vera <dfedericovera@gmail.com>
2023-04-20 18:10:54 -0300

    pass objects as props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from "./service/data"
+
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
